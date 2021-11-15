88d260ecacfd835a0fabf7e11dbf55f13c6e0a56
whl <972985083@qq.com>
2021-11-15 12:38:18 +0800

    up

--- src/DcatMediaSelectorServiceProvider.php
+++ src/DcatMediaSelectorServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace DeMemory\DcatMediaSelector;
 
+use Dcat\Admin\Admin;
 use Dcat\Admin\Extend\ServiceProvider;
 use Dcat\Admin\Form;
 use DeMemory\DcatMediaSelector\Commands\InstallCommand;
@@ -18,10 +19,6 @@ class DcatMediaSelectorServiceProvider extends ServiceProvider
 
     protected $js = [
         'js/index.js',
-        'https://unpkg.com/bootstrap-table@1.15.3/dist/bootstrap-table.min.js',
-        'https://unpkg.com/bootstrap-table@1.15.3/dist/locale/bootstrap-table-zh-CN.min.js',
-        'https://unpkg.com/bootstrap-table@1.15.3/dist/locale/bootstrap-table-en-US.min.js',
-        'https://cdn.bootcdn.net/ajax/libs/Sortable/1.14.0/Sortable.min.js',
     ];
 
     /**
@@ -31,13 +28,20 @@ class DcatMediaSelectorServiceProvider extends ServiceProvider
      */
     protected function registerPublishing()
     {
-        $this->loadViewsFrom(__DIR__.'/../resources/views', 'dcat-media-selector');
-        $this->publishes([__DIR__.'/../updates' => database_path('migrations')], 'dcat-media-selector-migrations');
-        $this->publishes([__DIR__.'/../resources/assets' => public_path('vendor/dcat-admin-extensions/de-memory/dcat-media-selector')], 'dcat-media-selector-assets');
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'dcat-media-selector');
+        $this->publishes([__DIR__ . '/../updates' => database_path('migrations')], 'dcat-media-selector-migrations');
+        $this->publishes([__DIR__ . '/../resources/assets' => public_path('vendor/dcat-admin-extensions/de-memory/dcat-media-selector')], 'dcat-media-selector-assets');
     }
 
     public function register()
     {
+        Admin::js([
+            'https://unpkg.com/bootstrap-table@1.15.3/dist/bootstrap-table.min.js',
+            'https://unpkg.com/bootstrap-table@1.15.3/dist/locale/bootstrap-table-zh-CN.min.js',
+            'https://unpkg.com/bootstrap-table@1.15.3/dist/locale/bootstrap-table-en-US.min.js',
+            'https://cdn.bootcdn.net/ajax/libs/Sortable/1.14.0/Sortable.min.js',
+        ]);
+
         Form::extend('mediaSelector', MediaSelector::class);
     }
 
